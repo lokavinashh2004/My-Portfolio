1d955512fdf60bc06b2832dc832a11d2c08f0ed6
Varadarajan M <varad2k12@gmail.com>
2024-08-15 00:23:07 +0530

    feat: experience tree complete

--- src/app/components/experience/ExperienceTree.tsx
+++ src/app/components/experience/ExperienceTree.tsx
@@ -8,12 +8,16 @@ interface ExperienceTreeProps {
 const ExperienceTree = ({ data }: ExperienceTreeProps) => {
   return (
   
-    <div className="relative z-30 flex flex-col gap-12 shadow-lg rounded-xl w-full">
+    <div className="relative z-30 flex flex-col gap-12 shadow-lg p-6 sm:p-6 rounded-xl w-full">
       {data.map((d,i) => {
         return (
-          <div className="flex flex-col gap-2" key={d.id}>
+          <div className={cn("before:top-0 relative before:rounded-md before:-left-[18px] after:left-[-23.5px] before:absolute flex flex-col gap-2 before:bg-[image:radial-gradient(75%_100%_at_50%_0%,_rgba(169,197,209,0.32)_0%,_rgba(255,255,255,0.08)_100%)] before:to-cyan-300 before:min-w-[1.5px] after:top-0 after:absolute after:min-w-[13px] after:min-h-[13px] after:bg-[#170d2b] after:border after:border-[#ffffff45] after:rounded-full hover:after:bg-cyan-600  before:transition-colors after:transition-colors after:duration-200",{
+            "before:min-h-[89%] after:mt-[10px] before:mt-[16px] ":i === data.length - 1,
+            "before:min-h-[calc(100%_+_50px)] before:mt-[16px] after:mt-[9px]": i < data.length - 1,
+            "after:bg-cyan-700": i === 0
+          })} key={d.id}>
             <div className="flex flex-wrap items-baseline gap-2">
-              <h3 className="font-light text-[#00bbfb] text-[16px] md:text-[22px]">{d.organisation}</h3>
+              <h3 className="selection:bg-white font-light text-[#00bbfb] text-[16px] md:text-[22px] selection:text-cyan-500">{d.organisation}</h3>
               <p className="text-gray-200 text-sm text-opacity-50">
                 {d.fromDate} - {d.toDate}
               </p>
@@ -23,8 +27,8 @@ const ExperienceTree = ({ data }: ExperienceTreeProps) => {
             <div className="flex flex-col gap-2">
               <p className="font-bold text-[#c3b8da] text-[22px] md:text-[28px]">{d.role}</p>
               {d.description?.map((item) => (
-                <p key={item} className="font-light text-[#97a4b7] text-sm">
-                  {item}
+                <p key={item} className="font-light text-[#9ba7b9] text-sm">
+                  <span className="before:inline-block before:[content:'✨'] before:opacity-70 before:mr-1">{item}</span>
                 </p>
               ))}
             </div>
--- src/app/components/experience/data.ts
+++ src/app/components/experience/data.ts
@@ -1,37 +1,55 @@
 const data = [
-    {
-        id:'genpro',
-        organisation:"Genpro Research",
-        fromDate: "Jul 2023",
-        toDate:'Current',
-        role:"Frontend Engineer",
-        description:[
-            'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incidt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo.'
-        ],
-        logo:'',
-    },
-    {
-        id:'infosys',
-        organisation:"Infosys Ltd.",
-        fromDate: "Dec 2021",
-        toDate:'Jul 2023',
-        role:"CyberSecurity Engineer",
-        description:[
-            'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incidt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo.'
-        ],
-        logo:''
-    },
-    {
-        id:'techrsr',
-        organisation:"Tech RSR",
-        fromDate: "May 2021",
-        toDate:'Dec 2021',
-        role:"Full-stack Engineer",
-        description:[
-            'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incidt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo.'
-        ],
-        logo:''
-    }
-]
+  {
+    id: "genpro",
+    organisation: "Genpro Research",
+    fromDate: "Jul 2023",
+    toDate: "Present",
+    role: "Frontend Engineer",
+    description: [
+      "Building the MAIA product suite, which is an authoring platform for the pharmaceutical sector that utilizes machine learning to significantly reduce authoring time from months to weeks and cut costs by up to 50%.",
+
+      "Built an advanced semantic search AI chat where medical writers can ask questions from multiple records. Implemented features including chat, follow-up, response regeneration, streaming response, adding contents along with citation info of the articles with a single click, and navigating to the correct page number of pdf or article from which the response is derived.",
+
+      "Contributed to the development of a rich text editor with SlateJS, implementing features like live sync of editor in multiple tabs, text formatting and font modifications similar to MS Word.",
+      `Built and published a UI component library (npm package) with highly scalable and accessible components (data
+tables, charts, searchable dropdowns, etc.) using Rollup.js, Storybook, Jest... ensuring design consistency across multiple products in MAIA suite, and reduced the UI development time in our products.            `,
+    ],
+    logo: "",
+  },
+  {
+    id: "infosys",
+    organisation: "Infosys Ltd.",
+    fromDate: "Dec 2021",
+    toDate: "Jul 2023",
+    role: "CyberSecurity Engineer",
+    description: [
+        "Worked on Security Orchestration Automation and Response (SOAR) team as a CyberSecurity Engineer.",
+        
+        "Collaborated with Security Analysts and clients to implement automated processes and playbooks, significantly improving the efficiency of incident management.",
+        
+        "Developed a user-friendly Incident Response Workbench in Cortex XSOAR, enhancing the analysis and handling of alerts from SIEM platforms like IBM QRadar and ticket management systems like ServiceNow. This improvement led to faster incident handling and reduced false positives."
+      ],
+    logo: "",
+  },
+  {
+    id: "techrsr",
+    organisation: "Tech RSR",
+    fromDate: "May 2021",
+    toDate: "Dec 2021",
+    role: "Full-stack Engineer",
+    description: [
+        "Played a key role in developing Handy Project Management (HPM) and Handy Meeting Minutes (HMM), leveraging technologies like React JS, Node JS, and TypeScript to build both front-end and back-end components. Enhanced user interfaces and integrated complex functionalities to streamline project management and meeting tracking.",
+        
+        "Improved application performance and efficiency by optimizing both front-end and back-end code, implementing best practices, and creating reusable components. This approach accelerated development and ensured consistency across applications.",
+        
+        "Integrated RESTful APIs and managed state with Redux on the front end, while also contributing to server-side logic and database interactions. Applied responsive design principles and ensured seamless functionality across devices.",
+        
+        "Automated integration testing for multiple products using Cypress and optimized search performance by reducing API requests through caching and debouncing. Enhanced user experience with features like autocomplete and suggestion boxes, addressing both client-side and server-side needs."
+      ]
+      
+      ,
+    logo: "",
+  },
+];
 
 export default data;
