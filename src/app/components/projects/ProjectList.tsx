84fa0d8eebcb88078ae4876ec4020195da4b1ff7
Lok Avinashh.T <lokavinashh2004@gmail.com>
2025-10-01 18:59:05 +0530

    Limit home page to display only 4 recent projects

--- src/app/components/projects/ProjectList.tsx
+++ src/app/components/projects/ProjectList.tsx
@@ -4,9 +4,12 @@ import ProjectCard from "./ProjectCard";
 import projects from "./data";
 
 const ProjectList = () => {
+  // Show only the first 4 projects on home page
+  const recentProjects = projects.slice(0, 4);
+  
   return (
     <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 w-full">
-      {projects.map((project) => (
+      {recentProjects.map((project) => (
         <ProjectCard key={project.id} project={project} />
       ))}
     </div>
